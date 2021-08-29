f0547abb20b6337d9bc3a58826f71aae5ffad4af
Myo Min Kyaw <m2k1661994@gmail.com>
2021-08-29 19:54:32 +0630

    add backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Homescreen from './screens/Homescreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <main className="py-3">
-        <Container>
-          <h1>Welcome to proshop</h1>
-        </Container>
-      </main>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <main className="py-3">
+          <Container>
+            <Route path='/' component={Homescreen} exact />
+            <Route path='/product/:id' component={ProductScreen}/>
+          </Container>
+        </main>
+        <Footer/>
+        </div>
+      </Router>
   )
 }
 
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container} from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" collapseOnSelect variant='dark' expand="lg">
                 <Container>
-                    <Navbar.Brand href="/">Shop</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <a herf="/">
+                        <Navbar.Brand >Shop</Navbar.Brand>
+                    </a>    
                     <Nav className="ml-auto">
-                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart m-2'></i>Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user m-2'></i>Sing In</Nav.Link>
+                        <a href="/cart">
+                            <Nav.Link><i className='fas fa-shopping-cart m-2'></i>Cart</Nav.Link>
+                        </a>
+                        <a href="/login">
+                            <Nav.Link><i className='fas fa-user m-2'></i>Sing In</Nav.Link>
+                        </a>
                     </Nav>
                 </Container>
             </Navbar>
